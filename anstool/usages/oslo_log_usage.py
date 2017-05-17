5ea2e7b9eabf9179f2130363ea000aa765ce3005
Yuancong Deng <yuancong.deng@ele.me>
2017-05-17 20:54:39 +0800

    stage

--- anstool/usages/oslo_log_usage.py
+++ anstool/usages/oslo_log_usage.py
@@ -1,6 +1,8 @@
+import sys
+
+import os
 from oslo_config import cfg
 from oslo_log import log as logging
-
 
 LOG = logging.getLogger(__name__)
 CONF = cfg.CONF
@@ -10,10 +12,19 @@ DOMAIN = "demo"
 
 options = logging.register_options(CONF)
 
+CONF(sys.argv[1:],
+     default_config_files=['ini/oslo_config_usage.ini'])
+
+if not os.path.exists(CONF.log_dir):
+    os.mkdir(CONF.log_dir)
+
+# with open(CONF.log_dir + "/" + CONF.log_file, 'a') as f:
+#     # f.write("")
+#     pass
 
 logging.setup(CONF, DOMAIN)
+
 
 LOG.info("oslo info log")
 LOG.warning("oslo warning log")
 LOG.error("oslo error log")
-
